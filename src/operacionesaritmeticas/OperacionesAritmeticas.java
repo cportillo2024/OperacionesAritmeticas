c446234c96fd78084192e6018c4660ecad28f1ac
CARLOS PORTILLO <CARLOS PORTILLO@PORTILLO>
2024-11-01 12:19:04 -0600

    inicio

---
+++ src/operacionesaritmeticas/OperacionesAritmeticas.java
@@ -0,0 +1,14 @@
+package operacionesaritmeticas;
+public class OperacionesAritmeticas {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicacion: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulo: " + (num1 % num2));
+    }
+}
+
